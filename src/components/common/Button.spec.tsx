b33ef38b0815d6458153847d72084f1fd4c60d89
Crow-yoo <dbwltkd5984@gmail.com>
2024-11-12 13:57:52 +0900

    #2. Button components, test completed.

---
+++ src/components/common/Button.spec.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react"
+
+import { BookStoreThemeProvider } from "../../context/themeContext";
+import Button from "./Button";
+
+describe("Button 컴포넌트 테스트", () => {
+    it("렌더를 확인", () => {
+        //1렌더
+        render(
+        <BookStoreThemeProvider>
+        <Button size="large" scheme="primary"> 버튼 </Button>
+        </BookStoreThemeProvider>
+        );
+
+        //2확인
+        expect(screen.getByText("버튼")).toBeInTheDocument();
+    });
+
+    it('size props 적용', () => {
+        const { container } = render(
+            <BookStoreThemeProvider>
+                <Button size="large" scheme="primary"> 버튼 </Button>
+            </BookStoreThemeProvider>
+        );
+        expect(screen.getByRole("button")).toHaveStyle({ fontSize: "1.5rem" });
+    });
+
+});
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
+import Button from "../components/common/Button";
+import Title from "../components/common/Title";
 
 
 function Home() {
     return (
     <>
-        
+        <Title size="medium" color="background">
+            제목테스트
+        </Title>
+        <Button size="large" scheme="primary"> 
+            버튼 테스트 
+        </Button>
         <div>Home Body</div>
         
     </>
